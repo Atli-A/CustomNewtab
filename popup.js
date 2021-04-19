ffb41982b204c4651585567eac3fb55ccad7602e
Atli Arnarsson <atli.nikulas@gmail.com>
2021-04-19 15:30:29 -0700

    experimenting with popup

--- popup.js
+++ popup.js
@@ -1,94 +1,10 @@
 
+chrome.storage.sync.set({"titleText":"default text!"}, function() {
+	
+});
 
 
-document.addEventListener("DOMContentLoaded", function () {
+titleVal = chrome.storage.sync.get(["titleText"], function(items) {
+	console.log(items["titleText"])
+})
 
-
-    // console.log(storageGet)
-
-    // if (chrome.storage.sync.get(['darkScroll'], function (result) {}) == undefined) {
-    //     chrome.storage.sync.set({ darkScroll: false })
-    // }
-
-    // scrollbarCustom = chrome.storage.sync.get(['darkScroll'], function() {})
-
-
-    // function storageGet(name) {
-    //     return chrome.storage.sync.get(['darkScroll'], function (result) {});
-    // }
-
-    let scrollCheckbox = document.getElementById("changeScrollbar")
-    scrollCheckbox.addEventListener("change", customizeScrollBar, false)
-
-
-
-    function customizeScrollBar() {
-        chrome.tabs.query({ currentWindow: true, active: true },
-            function (tabs) {
-                // chrome.tabs.sendMessage(tabs[0].id, "hi")
-            })
-        if (scrollCheckbox.checked) {
-            console.log("test")
-            document.getElementById("followUpScrollbar").innerHTML = '<label for="darkScrollbar">  <input type="checkbox" id="darkScrollbar" name="darkScrollbar"> Dark Scrollbar</label>'
-            scrollBarColorUpdate()
-            document.getElementById("darkScrollbar").addEventListener("change", scrollBarColorUpdate, true)
-
-
-
-            //adding rounded scrollbars
-            document.getElementById("followUpScrollbar").innerHTML += '<label for="roundedScrollbar">  <input type="checkbox" id="roundedScrollbar" name="roundedScrollbar"> Rounded Scrollbar</label>'
-            scrollBarColorUpdate()
-            document.getElementById("roundedScrollbar").addEventListener("change", scrollBarRoundingUpdate, true)
-
-        }
-        else {
-            document.getElementById("followUpScrollbar").innerHTML = ''
-
-        }
-
-
-        // alert("yeet")
-    }
-
-    function scrollBarRoundingUpdate() {
-        if (document.getElementById("roundedScrollbar") != null) {
-            if (document.getElementById("roundedScrollbar").checked) {
-                chrome.tabs.insertCSS({ file: "css/roundedScrollbar.css" });
-            }
-            else {
-                chrome.tabs.insertCSS({ file: "css/unroundedScrollbar.css" });
-
-            }
-        }
-        else {
-            console.log("darkScrollbar not found")
-        }
-    }
-
-    function scrollBarColorUpdate() {
-        console.log("changing color")
-        if (document.getElementById("darkScrollbar") != null) {
-            if (document.getElementById("darkScrollbar").checked) {
-                chrome.tabs.insertCSS({ file: "css/darkScrollbar.css" });
-            }
-            else {
-                chrome.tabs.insertCSS({ file: "css/lightScrollbar.css" });
-
-            }
-        }
-        else {
-            console.log("darkScrollbar not found")
-        }
-
-    }
-
-}, false)
-
-
-// chrome.storage.sync.set({ key: value }, function () {
-//     console.log('Value is set to ' + value);
-// });
-
-// chrome.storage.sync.get(['darkScroll'], function (result) {
-//     console.log('Value currently is ' + result.key);
-// });
--- script.js
+++ script.js
@@ -1,14 +1,34 @@
 
 
-var interval = setInterval(clockSet, 100);
+var interval = setInterval(updateAll, 100);
 var d = new Date()
-
-function clockSet() {
+let count = 0;
+function updateAll() {
     d = new Date() // this code isn't great but it does work
     document.getElementById("clock").innerHTML = ('00' + d.getHours()).substr(-2) + ":" + ('00' + d.getMinutes()).substr(-2) + "<span id='Seconds'>" + ":" + ('00' + d.getSeconds()).substr(-2) + "</span>"
         + "<span id='Milliseconds'>" + ":" + String(d.getMilliseconds()).substr(0, 1) + "</span>"
-}
 
+
+let y = document.title;
+    switch(count % 8) {
+	case 0:
+	    y = "|"
+	case 1:
+	    y = "/"
+	case 2:
+	    y = "-"
+	case 3:
+	    y = "\\"
+	case 4:
+	    y = "|"
+	case 5:
+	    y = "/"
+	case 6:
+	    y = "-"
+	case 7:
+	    y = "\\"
+     }
+}
 
 document.getElementById("replacementSearchBar").onkeypress = function (e) {
     if (!e) e = window.event;
@@ -18,4 +38,6 @@ document.getElementById("replacementSearchBar").onkeypress = function (e) {
         window.location.replace("https://www.google.com/search?q=" + x);
     }
 }
+
+
 
