0f1b8885afdef1293f07710a78de24b501580fb7
Atli Arnarsson <atli.nikulas@gmail.com>
2021-04-19 23:46:18 -0700

    breaking changes

--- popup.js
+++ popup.js
@@ -1,3 +1,19 @@
+//generates link config
+let links = []
+let shorthands = []
+for (let i = 0; i < 10; i++) {
+	links.push("l" + i)
+	shorthands.push("s" + i)
+}
+console.log("links = " + links)
+console.log("shorthands = " + shorthands)
+
+let shorthands_div = document.getElementById("shorthand")
+let links_div = document.getElementById("links")
+for (let i = 0; i < links.length; i++) {
+	// shorthands_div.appendChild(document.createElement("input", {}));
+	shorthands_div.innerHTML += '<input name="title" type="text"></input>'
+}
 
 titleVal = chrome.storage.sync.get(["titleText"], function(items) { // set value of input to what it already is
 	document.getElementById("popupTitle").value = items["titleText"]
@@ -10,6 +26,7 @@ document.getElementById("popupTitle").onkeypress = function(e) { // for any keyp
 		chrome.storage.sync.set({"titleText":document.getElementById("popupTitle").value}, function() {
 			
 		});
+		window.close()
 	}
 }
 
